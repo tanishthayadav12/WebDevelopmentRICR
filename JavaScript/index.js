3acb1b967e30ebc25951121ed119f32839c2472d
tanishthayadav12 <tanishthayadav12@gmail.com>
2025-11-16 16:57:17 +0530

    JavaScript Basics

---
+++ JavaScript/index.js
@@ -0,0 +1,25 @@
+var a = 10;
+
+var a = 20;
+
+let b = 10;
+
+// let b=20; error in let duplicate
+
+console.log(a, b);
+
+let ar=["Abc",1235,45.67,'a',true];
+console.log(ar);
+
+console.warn("this is warning");
+console.error("ye error hai");
+
+alert("this is alert");
+
+if(confirm("Are you sure u want to confirm"))
+{
+    console.log("thanks");
+}
+else{
+    console.error("Its ok");
+}
