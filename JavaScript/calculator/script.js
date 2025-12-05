41011de2cab3deeb1d1d91ff054fdc90107db372
tanishthayadav12 <tanishthayadav12@gmail.com>
2025-12-05 19:49:37 +0530

    Calculator Button Event

--- JavaScript/calculator/script.js
+++ JavaScript/calculator/script.js
@@ -15,3 +15,30 @@ function bt(char) {
       document.getElementById("display").value + char;
   }
 }
+
+document.addEventListener("keydown", (abc) => {
+  console.log(abc.key);
+
+  if (abc.key === "Enter") {
+    bt("=");
+  } else if (
+    abc.key === "1" ||
+    abc.key === "2" ||
+    abc.key === "3" ||
+    abc.key === "4" ||
+    abc.key === "5" ||
+    abc.key === "6" ||
+    abc.key === "7" ||
+    abc.key === "8" ||
+    abc.key === "9" ||
+    abc.key === "10" ||
+    abc.key === "+" ||
+    abc.key === "-" ||
+    abc.key === "*" ||
+    abc.key === "/"
+  ) {
+    bt(abc.key);
+  } else if (abc.key === "Backspace") {
+    bt("clear");
+  }
+});
