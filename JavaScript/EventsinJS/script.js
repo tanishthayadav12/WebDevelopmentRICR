35fefe65521f989fd1c66235ebd64e2a69447d5c
tanishthayadav12 <tanishthayadav12@gmail.com>
2025-12-05 20:31:22 +0530

    Bulb Hover Event

--- JavaScript/EventsinJS/script.js
+++ JavaScript/EventsinJS/script.js
@@ -18,7 +18,7 @@ function blue() {
   document.getElementById("bulb").style.backgroundColor = "blue";
 }
 
-const userColor = document.getElementById("color"); 
+const userColor = document.getElementById("color");
 userColor.addEventListener("change", () => changeBulbColor(userColor.value));
 
 function changeBulbColor(color) {
@@ -38,4 +38,66 @@ function SB_control() {
 
 function SB_control2() {
   document.getElementById("smartBulb").classList.toggle("on");
+}
+
+document.getElementById("c1").addEventListener("mouseenter", () => {
+  fillColor("violet");
+});
+
+document.getElementById("c1").addEventListener("mouseleave", () => {
+  fillColor("white");
+});
+
+document.getElementById("c2").addEventListener("mouseenter", () => {
+  fillColor("indigo");
+});
+
+document.getElementById("c2").addEventListener("mouseleave", () => {
+  fillColor("white");
+});
+
+document.getElementById("c3").addEventListener("mouseenter", () => {
+  fillColor("blue");
+});
+
+document.getElementById("c3").addEventListener("mouseleave", () => {
+  fillColor("white");
+});
+
+document.getElementById("c4").addEventListener("mouseenter", () => {
+  fillColor("green");
+});
+
+document.getElementById("c4").addEventListener("mouseleave", () => {
+  fillColor("white");
+});
+
+document.getElementById("c5").addEventListener("mouseenter", () => {
+  fillColor("yellow");
+});
+
+document.getElementById("c5").addEventListener("mouseleave", () => {
+  fillColor("white");
+});
+
+document.getElementById("c6").addEventListener("mouseenter", () => {
+  fillColor("orange");
+});
+
+document.getElementById("c6").addEventListener("mouseleave", () => {
+  fillColor("white");
+});
+
+document.getElementById("c7").addEventListener("mouseenter", () => {
+  fillColor("red");
+});
+
+document.getElementById("c7").addEventListener("mouseleave", () => {
+  fillColor("white");
+});
+
+function fillColor(color) {
+  console.log(color);
+
+  document.getElementById("rainbowBulb").style.backgroundColor = color;
 }
