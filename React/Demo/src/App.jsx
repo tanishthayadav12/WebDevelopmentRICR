280461a83068d875910a5e3e89a3d910b5037423
Tanishtha Yadav <tanishthayadav12@gmail.com>
2025-12-25 14:48:58 +0530

    Demo App

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  let b = 10;
+
+  console.log(a + b);
+
+  return (
+    <>
+      <h1 id="head">Hello world</h1>
+      <div>This is my first React app</div>
+    </>
+  );
+}
+
+export default App;
